128de93f1dafcd5e73ea4184bbd85ee2b443e665
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-09-26 10:28:47 +0530

    Added Image Input and Button

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [img, setImg] = useState();
+  const imgRef = useRef();
+
+  const onProcessHandler = () => {
+    console.log("Process");
+  };
+
+  const onImgClick = () => {
+    imgRef?.current?.click();
+
+    setImg(imgRef?.current?.value);
+    console.log(imgRef?.current?.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          ref={imgRef}
+          type="file"
+          accept="image/png image/jpeg"
+          alt="Select Image"
+          name="img"
+          id="img"
+          // value={img}
+          style={{
+            visibility: "hidden",
+          }}
+        />
+
+        <div>
+          {img ? (
+            <img src={img} className="App-logo" alt="logo" />
+          ) : (
+            <p>Select Image</p>
+          )}
+        </div>
+
+        <div>
+          <button
+            onClick={onImgClick}
+            style={{
+              color: "white",
+              background: "#62DAFB",
+              padding: "0.75rem",
+              fontWeight: "bold",
+              border: "none",
+              margin: "1rem",
+            }}
+          >
+            SELECT IMAGE
+          </button>
+
+          <button
+            onClick={onProcessHandler}
+            style={{
+              color: "white",
+              background: "#62DAFB",
+              padding: "0.75rem",
+              fontWeight: "bold",
+              border: "none",
+              margin: "1rem",
+            }}
+          >
+            PROCESS
+          </button>
+        </div>
       </header>
     </div>
   );
