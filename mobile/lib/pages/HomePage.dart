2ee83160e7492a6fe5a9ceea4ea1941c6d56cc12
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-09-26 09:20:31 +0530

    Added Button and Image Component

--- mobile/lib/pages/HomePage.dart
+++ mobile/lib/pages/HomePage.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,7 +11,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void handleImageUpload() {}
+  final ImagePicker _picker = ImagePicker();
+  XFile? _imgFile;
+
+  void handleImageUpload() async {
+    XFile? file = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (file != null) {
+      debugPrint(file.path);
+
+      setState(() {
+        _imgFile = file;
+      });
+    } else {
+      debugPrint("No Image found!");
+    }
+  }
+
+  onClickHandler() {
+    debugPrint("Processed...");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +41,29 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            _imgFile != null
+                ? Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Image.file(File(_imgFile!.path)),
+                  )
+                : const Text("Select Image"),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: TextButton(
+                onPressed: _imgFile != null ? onClickHandler : null,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.blueAccent,
+                  ),
+                ),
+                child: const Text(
+                  "PROCESS",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
